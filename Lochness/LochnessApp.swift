9c1783434b612eb5ca787eda611e3b03a3a89e62
Desmond Fitch <desmond.fitch@icloud.com>
2023-12-23 08:18:24 -0500

    Initial Commit

---
+++ Lochness/LochnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  LochnessApp.swift
+//  Lochness
+//
+//  Created by Desmond Fitch on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct LochnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
